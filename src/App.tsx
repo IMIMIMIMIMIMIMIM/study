2d072d0146825a63c91fdbb23295b1a367d4917e
IMIMIMIMIMIMIMIM <ism1124@naver.com>
2025-03-21 15:32:07 +0900

    study

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline item-middle">Hello world!</h1>
+  );
 }
